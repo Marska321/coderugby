8e89efb80a96c4f36881c8ee7aa9e3d7c6099f33
Marska321 <mario.botha.mb@gmail.com>
2025-07-15 10:50:25 +0200

    Create firebase-config.js

---
+++ public/firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBB1RmG4tMM7MdlSvxSEm4uVodcjBnupTo",
+  authDomain: "python-rugby-coding.firebaseapp.com",
+  projectId: "python-rugby-coding",
+  storageBucket: "python-rugby-coding.firebasestorage.app",
+  messagingSenderId: "126358162035",
+  appId: "1:126358162035:web:23139b87ca2cab282ef806"
+};
